7663e59aa0f09c9b57d53737b77261420d1f4b86
yuke001 <yukenthirans2018@gmail.com>
2025-02-05 00:30:24 +0530

    add dotenv config

--- todo-backend/app.js
+++ todo-backend/app.js
@@ -2,6 +2,26 @@ import express from "express";
 import connectDB from "./config/db.js";
 import todoRouter from "./routes/todoRoutes.js";
 import cors from "cors";
+import * as dotenv from "dotenv"; 
+
+dotenv.config(); 
+
+// Configure CORS based on environment
+const isProduction = process.env.NODE_ENV === "production";
+const allowedOrigins = isProduction
+  ? [process.env.FRONTEND_URL]
+  : ["http://localhost:5173"]; // Replace with your Vercel URL
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Keep this line if you want to explicitly specify the allowed methods
+};
 
 let app = express();
 
@@ -10,7 +30,8 @@ connectDB();
 
 // middleware
 app.use(express.json());
-app.use(cors());
+
+app.use(cors(corsOptions));
 
 //routes
 app.use("/api", todoRouter);
